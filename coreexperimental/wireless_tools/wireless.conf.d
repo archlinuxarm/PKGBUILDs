6b6740558592912d2596924e289d8e8d0eedce6e
Mike Staszel <Mike@Mike-Staszels-Mac.local>
2009-10-01 17:48:22 -0500

    Minor cleanup, Added Experimental Core PKGBUILDs for a test

---
+++ coreexperimental/wireless_tools/wireless.conf.d
@@ -0,0 +1,12 @@
+#
+# Settings for wireless cards
+#
+# For each wireless interface declared in INTERFACES (in rc.conf), declare
+# a wlan_${IF} variable that contains the arguments to be passed to
+# iwconfig(8).  Then list the original interface name in the
+# WLAN_INTERFACES array.
+#
+
+#wlan_eth0="eth0 mode managed essid default"
+#WLAN_INTERFACES=(eth0)
+
