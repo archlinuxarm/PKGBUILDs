ec80ace66c4f2e2b05e84d2b61791bcb3753725f
Mike Staszel <Mike@Mike-Staszels-Mac.local>
2009-10-09 22:25:07 -0500

    Core cleanup

--- community/abyssws/abyssws.conf.d
+++
@@ -1,9 +0,0 @@
-OPTIONS=""
-# -c  Specify an alternate configuration file.
-#     If the file does not exist, it is created and filled
-#     with the default options values.
-#
-# -p  Specify an alternate console port.
-#     It overrides the console port value in the configuration file.
-
-
