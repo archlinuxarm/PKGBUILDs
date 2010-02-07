7623231e4285a88e720ca9b34477b169882c4035
root <root@sheevaplug.(none)>
2010-02-06 23:10:55 -0600

    Added GPLv2 declaration to papps_installer

--- scripts/papps_installer.sh
+++ scripts/papps_installer.sh
@@ -1,9 +1,21 @@
 #! /bin/bash
 
 #
-# Papps_Installer for Plugapps
-# Works on *all* Plug devices (Pogoplug v1/2; Dockstar; Tonidoplug)
-# Version 0.5.1 # Halfie (becomes 1.0 upon testing)
+#   Papps_Installer for Plugapps
+#   Version 0.5.2 # Halfie (becomes 1.0 upon testing)
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
 ###########################
