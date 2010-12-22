c8dd10ca6a7020e983d8b6cf1dc643a79bb4ac03
Mike Staszel <mikestaszel@gmail.com>
2010-12-22 14:36:48 -0600

    Moved all aur packages to [aur]

--- aur/sabnzbd/addnzb.sh
+++ aur/sabnzbd/addnzb.sh
@@ -4,10 +4,10 @@
 
 # SABnzbd must be able to read the file, so we copy it to a directory where it
 # certainly has rights.
-nzbname=`basename $1`
-TEMP_NZB="/var/tmp/"$nzbname
+nzbname=`basename "$1"`
+TEMP_NZB="/var/tmp/$nzbname"
 
-cp $1 $TEMP_NZB
+cp "$1" $TEMP_NZB
 curl -f "$SABNZBD_PROTOCOL://$SABNZBD_USPW$SABNZBD_IP:$SABNZBD_PORT/sabnzbd/api?mode=addlocalfile&name=$TEMP_NZB&pp=1&priority=-1&apikey=$SABNZBD_KEY"
 rm $TEMP_NZB
 exit 0
