c82afc839eeae7e3a62e691f3e57063870ccfe16
Kevin Mihelich <kevin@plugapps.com>
2011-01-16 16:46:26 -0500

    Added community/distcc

---
+++ community/distcc/distccd.conf.d
@@ -0,0 +1,8 @@
+#
+# Parameters to be passed to distccd
+#
+# You must explicitly add IPs (or subnets) that are allowed to connect,
+# using the --allow switch.  See the distccd manpage for more info.
+#
+DISTCC_ARGS="--user nobody --allow 127.0.0.1"
+
