402d9f7c752e7e91001fe8bc942e81cab1e1b865
Kevin Mihelich <kevin@plugapps.com>
2011-04-13 00:10:55 -0400

    extra/aufs2 to 2.6.38_20110314-4

--- extra/aufs2/create-tarball.sh
+++ extra/aufs2/create-tarball.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-AUFS2VERSION="-37"
-KERNELVERSION=2.6.37
-GITSNAPSHOT=20110124
+AUFS2VERSION="-38"
+KERNELVERSION=2.6.38
+GITSNAPSHOT=20110314
 # aufs2 (no -xx) for the latest -rc version.
 git clone http://git.c3sl.ufpr.br/pub/scm/aufs/aufs2-standalone.git aufs2-standalone.git
 cd aufs2-standalone.git
-git checkout origin/aufs2.1${AUFS2VERSION}
-#git checkout origin/aufs2.1
+#git checkout origin/aufs2.1${AUFS2VERSION}
+git checkout origin/aufs2.1
 #*** apply "aufs2-base.patch" and "aufs2-standalone.patch" to your kernel source files.
 cd ..
 rm -rf aufs2-${KERNELVERSION}_${GITSNAPSHOT}
