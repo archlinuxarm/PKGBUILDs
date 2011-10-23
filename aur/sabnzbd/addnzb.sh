2b5c100245eebea1410b5cb1f8cce46bc494a738
Kevin Mihelich <kevin@plugapps.com>
2011-10-23 15:30:03 -0400

    fixes for aur/sabnzbd

--- aur/sabnzbd/addnzb.sh
+++ aur/sabnzbd/addnzb.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+set -a # export all variables
 
 . /etc/conf.d/sabnzbd
 
 # SABnzbd must be able to read the file, so we copy it to a directory where it
 # certainly has rights.
-nzbname=`basename "$1"`
+nzbname=$(basename "$1")
 TEMP_NZB="/var/tmp/$nzbname"
 
-cp "$1" $TEMP_NZB
-curl -f "$SABNZBD_PROTOCOL://$SABNZBD_USPW$SABNZBD_IP:$SABNZBD_PORT/sabnzbd/api?mode=addlocalfile&name=$TEMP_NZB&pp=1&priority=-1&apikey=$SABNZBD_KEY"
-rm $TEMP_NZB
+curl -s $(python2 -c 'import urlparse,urllib,sys; print sys.argv[1] if urlparse.urlparse(sys.argv[1]).scheme else urlparse.urlparse(urllib.pathname2url(sys.argv[1]),"file").geturl()' "$1") -o "$TEMP_NZB"
+curl -f $(python2 -c 'import urllib,os; print os.environ["SABNZBD_PROTOCOL"]+"://"+urllib.quote(os.environ["SABNZBD_USPW"]+os.environ["SABNZBD_IP"])+":"+os.environ["SABNZBD_PORT"]+"/sabnzbd/api?mode=addlocalfile&name="+urllib.quote(os.environ["TEMP_NZB"])+"&apikey="+urllib.quote(os.environ["NZB_KEY"])')
+rm "$TEMP_NZB"
 exit 0
