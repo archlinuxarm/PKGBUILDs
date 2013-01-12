e37cdd8d5565b2ed316da87892a7b5ef95c802a5
Kevin Mihelich <kevin@archlinuxarm.org>
2013-01-12 00:24:04 +0000

    aur updates

--- aur/sabnzbd/addnzb.sh
+++ aur/sabnzbd/addnzb.sh
@@ -1,14 +1,4 @@
-#!/bin/bash
-set -a # export all variables
+#!/bin/sh
 
-. /etc/conf.d/sabnzbd
-
-# SABnzbd must be able to read the file, so we copy it to a directory where it
-# certainly has rights.
-nzbname=$(basename "$1")
-TEMP_NZB="/var/tmp/$nzbname"
-
-curl -s $(python2 -c 'import urlparse,urllib,sys; print sys.argv[1] if urlparse.urlparse(sys.argv[1]).scheme else urlparse.urlparse(urllib.pathname2url(sys.argv[1]),"file").geturl()' "$1") -o "$TEMP_NZB"
-curl -f $(python2 -c 'import urllib,os; print os.environ["SABNZBD_PROTOCOL"]+"://"+urllib.quote(os.environ["SABNZBD_USPW"]+os.environ["SABNZBD_IP"])+":"+os.environ["SABNZBD_PORT"]+"/sabnzbd/api?mode=addlocalfile&name="+urllib.quote(os.environ["TEMP_NZB"])+"&apikey="+urllib.quote(os.environ["NZB_KEY"])')
-rm "$TEMP_NZB"
-exit 0
+. /etc/conf.d/sabnzbd 
+curl -s -F apikey="$API_KEY" -F mode="addfile" -F name=@"$1" $URL/sabnzbd/api &> /dev/null
