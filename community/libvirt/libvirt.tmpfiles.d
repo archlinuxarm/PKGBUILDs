c514a8beca79daf26fc141da4ad051705a4fe2bb
Kevin Mihelich <kevin@archlinuxarm.org>
2013-07-01 23:28:11 +0000

    added community/libvirt

---
+++ community/libvirt/libvirt.tmpfiles.d
@@ -0,0 +1,4 @@
+d /run/libvirt/qemu 0755 root root -
+d /run/libvirt/lxc 0755 root root -
+d /run/libvirt/uml 0755 root root -
+d /run/libvirt/network 0755 root root -
---
+++ community/libvirt/libvirtd.conf.d
@@ -0,0 +1,3 @@
+LIBVIRTD_CONFIG="/etc/libvirt/libvirtd.conf"
+LIBVIRTD_ARGS="-p /var/run/libvirtd.pid"
+KRB5_KTNAME="/etc/libvirt/krb5.tab"
