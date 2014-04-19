6f145edee7dfa344dea605ef04859022fba484fe
Kevin Mihelich <kevin@archlinuxarm.org>
2014-04-19 20:17:29 +0000

    added extra/libreoffice

---
+++ extra/libreoffice/libreoffice-common.csh
@@ -0,0 +1,6 @@
+# to force a certain look'n feel
+
+#setenv SAL_USE_VCLPLUGIN gen
+#setenv SAL_USE_VCLPLUGIN kde4
+#setenv SAL_USE_VCLPLUGIN gtk
+#setenv SAL_USE_VCLPLUGIN gtk3
---
+++ extra/libreoffice/libreoffice-common.sh
@@ -0,0 +1,6 @@
+# to force a certain look'n feel
+
+#export SAL_USE_VCLPLUGIN=gen
+#export SAL_USE_VCLPLUGIN=kde4
+#export SAL_USE_VCLPLUGIN=gtk
+#export SAL_USE_VCLPLUGIN=gtk3
