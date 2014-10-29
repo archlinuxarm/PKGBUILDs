e33228b6dca91a1e95b4c1ba6dfb63183dbf492e
Kevin Mihelich <kevin@archlinuxarm.org>
2014-10-29 13:52:08 +0000

    core/filesystem to 2014.10-3

--- core/filesystem/locale.sh
+++ core/filesystem/locale.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
-unset LANG LC_CTYPE LC_NUMERIC LC_TIME LC_COLLATE LC_MONETARY LC_MESSAGES \
-      LC_PAPER LC_NAME LC_ADDRESS LC_TELEPHONE LC_MEASUREMENT LC_IDENTIFICATION
-
-if [ -n "$XDG_CONFIG_HOME" ] && [ -r "$XDG_CONFIG_HOME/locale.conf" ]; then
-  . "$XDG_CONFIG_HOME/locale.conf"
-elif [ -n $HOME ] && [ -r $HOME/.config/locale.conf ]; then
-  . "$HOME/.config/locale.conf"
-elif [ -r /etc/locale.conf ]; then
-  . /etc/locale.conf
+if [ -z "$LANG" ]; then
+  if [ -n "$XDG_CONFIG_HOME" ] && [ -r "$XDG_CONFIG_HOME/locale.conf" ]; then
+    . "$XDG_CONFIG_HOME/locale.conf"
+  elif [ -n $HOME ] && [ -r $HOME/.config/locale.conf ]; then
+    . "$HOME/.config/locale.conf"
+  elif [ -r /etc/locale.conf ]; then
+    . /etc/locale.conf
+  fi
 fi
 
 LANG=${LANG:-C}
