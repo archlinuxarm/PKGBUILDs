446d4284b38df11e1527051445c9259c4628713c
Kevin Mihelich <kevin@archlinuxarm.org>
2015-02-20 13:14:29 +0000

    extra/chromium to 40.0.2214.115-1

--- extra/chromium/chromium.sh
+++ extra/chromium/chromium.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
 # Allow users to override command-line options
-# Based on Gentoo's chromium package (and by extension, Debian's)
+# Based on Gentoo's chromium package
 if [[ -f /etc/chromium/default ]]; then
 	. /etc/chromium/default
 fi
+# Source additional configuration files
+for file in /etc/chromium/*; do
+	# Don't source /etc/chromium/default again
+	[[ $file == /etc/chromium/default ]] && continue
+
+	if [[ -f $file ]]; then
+		. $file
+	fi
+done
 
 # Prefer user defined CHROMIUM_USER_FLAGS (from env) over system
 # default CHROMIUM_FLAGS (from /etc/chromium/default)
