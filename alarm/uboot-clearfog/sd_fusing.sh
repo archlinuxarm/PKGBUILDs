b44006b40e105ab459dddb6cb8932e5dced3513a
moonman <moonman.ca@gmail.com>
2015-12-11 23:47:38 -0700

    alarm/uboot-clearfog to 2013.01-2

--- alarm/uboot-clearfog/sd_fusing.sh
+++ alarm/uboot-clearfog/sd_fusing.sh
@@ -32,11 +32,11 @@ fi
 # fusing images
 
 echo "u-boot fusing"
-dd if=/boot/u-boot-clearfog.mmc of=/dev/mmcblk0 bs=512 seek=1
+dd if=./u-boot-clearfog.mmc of=$1 bs=512 seek=1
 
 echo "u-boot environemnt fusing"
-dd if=/dev/zero of=/dev/mmcblk0 bs=512 seek=1920 count=128
-dd if=/boot/clearfog.env of=/dev/mmcblk0 bs=512 seek=1920
+dd if=/dev/zero of=$1 bs=512 seek=1920 count=128
+dd if=./clearfog.env of=$1 bs=512 seek=1920
 
 ####################################
 #<Message Display>
