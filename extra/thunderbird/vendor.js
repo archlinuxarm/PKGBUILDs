59d7a520551535d0ddec65d0b99b9d13955f0054
Kevin Mihelich <kevin@archlinuxarm.org>
2016-05-15 14:54:52 +0000

    extra/thunderbird to 45.1.0-2

--- extra/thunderbird/vendor.js
+++ extra/thunderbird/vendor.js
@@ -7,3 +7,6 @@ pref("mail.shell.checkDefaultMail", false);
 // Don't disable our bundled extensions in the application directory
 pref("extensions.autoDisableScopes", 11);
 pref("extensions.shownSelectionUI", true);
+
+// https://bugzilla.mozilla.org/show_bug.cgi?id=1245783
+pref("javascript.options.ion", false);
