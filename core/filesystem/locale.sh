a184180aea0dcce05d25017d5552d85a627ad0cf
Kevin Mihelich <kevin@archlinuxarm.org>
2018-03-11 18:56:59 +0000

    core/filesystem to 2018.1-2

--- core/filesystem/locale.sh
+++ core/filesystem/locale.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# load locale.conf in XDG paths.
+# /etc/locale.conf loads and overrides by kernel command line is done by systemd
+# But we override it here, see FS#56688
 if [ -z "$LANG" ]; then
   if [ -n "$XDG_CONFIG_HOME" ] && [ -r "$XDG_CONFIG_HOME/locale.conf" ]; then
     . "$XDG_CONFIG_HOME/locale.conf"
@@ -10,17 +13,10 @@ if [ -z "$LANG" ]; then
   fi
 fi
 
+# define default LANG to C if not already defined
 LANG=${LANG:-C}
-export LANG
-[ -n "$LC_CTYPE" ]          && export LC_CTYPE
-[ -n "$LC_NUMERIC" ]        && export LC_NUMERIC
-[ -n "$LC_TIME" ]           && export LC_TIME
-[ -n "$LC_COLLATE" ]        && export LC_COLLATE
-[ -n "$LC_MONETARY" ]       && export LC_MONETARY
-[ -n "$LC_MESSAGES" ]       && export LC_MESSAGES
-[ -n "$LC_PAPER" ]          && export LC_PAPER
-[ -n "$LC_NAME" ]           && export LC_NAME
-[ -n "$LC_ADDRESS" ]        && export LC_ADDRESS
-[ -n "$LC_TELEPHONE" ]      && export LC_TELEPHONE
-[ -n "$LC_MEASUREMENT" ]    && export LC_MEASUREMENT
-[ -n "$LC_IDENTIFICATION" ] && export LC_IDENTIFICATION
+
+# export all locale (7) variables when they exist
+export LANG LANGUAGE LC_CTYPE LC_NUMERIC LC_TIME LC_COLLATE LC_MONETARY \
+       LC_MESSAGES LC_PAPER LC_NAME LC_ADDRESS LC_TELEPHONE LC_MEASUREMENT \
+       LC_IDENTIFICATION
