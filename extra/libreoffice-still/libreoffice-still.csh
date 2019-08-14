b79e67f96babc49255ba2879c0ac6cf4da33d2ff
Kevin Mihelich <kevin@archlinuxarm.org>
2019-08-14 23:58:01 +0000

    extra/libreoffice-still to 6.2.6-1

--- extra/libreoffice-still/libreoffice-still.csh
+++ extra/libreoffice-still/libreoffice-still.csh
@@ -1,5 +1,8 @@
 # to force a certain look'n feel
 
 #setenv SAL_USE_VCLPLUGIN gen
+#setenv SAL_USE_VCLPLUGIN gtk3_kde5
+#setenv SAL_USE_VCLPLUGIN kde5
+#setenv SAL_USE_VCLPLUGIN qt5
 #setenv SAL_USE_VCLPLUGIN gtk
 #setenv SAL_USE_VCLPLUGIN gtk3
--- extra/libreoffice-still/libreoffice-still.sh
+++ extra/libreoffice-still/libreoffice-still.sh
@@ -1,5 +1,8 @@
 # to force a certain look'n feel
 
 #export SAL_USE_VCLPLUGIN=gen
+#export SAL_USE_VCLPLUGIN=gtk3_kde5
+#export SAL_USE_VCLPLUGIN=kde5
+#export SAL_USE_VCLPLUGIN=qt5
 #export SAL_USE_VCLPLUGIN=gtk
 #export SAL_USE_VCLPLUGIN=gtk3
