aaad3dd1b84f5f1b5c1f2b1e1ceeeb407d00c153
Kevin Mihelich <kevin@archlinuxarm.org>
2020-02-04 04:33:26 +0000

    extra/gn to 0.1634.ad9e442d-1

--- extra/gn/chromium-gn-version.sh
+++ extra/gn/chromium-gn-version.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-chromium_version=$(curl -s https://omahaproxy.appspot.com/linux)
+chromium_version=${1:-$(curl -s https://omahaproxy.appspot.com/linux)}
 
-curl -s https://chromium.googlesource.com/chromium/src/+/refs/tags/$chromium_version/DEPS?format=TEXT |
+curl -s https://chromium.googlesource.com/chromium/src/+/$chromium_version/DEPS?format=TEXT |
 	base64 -d | grep -Po "'gn_version': 'git_revision:\K[^']*"
