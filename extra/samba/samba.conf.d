936e1bdadc3008aee3d84bf40815c30e9522299d
Kevin Mihelich <kevin@archlinuxarm.org>
2020-04-29 18:09:46 +0000

    added extra/samba

---
+++ extra/samba/samba.conf.d
@@ -0,0 +1,18 @@
+## Path:           Network/Samba
+## Description:    Samba process options
+## Type:           string
+## Default:        ""
+## ServiceRestart: samba
+SAMBAOPTIONS=""
+## Type:           string
+## Default:        ""
+## ServiceRestart: smb
+SMBDOPTIONS=""
+## Type:           string
+## Default:        ""
+## ServiceRestart: nmb
+NMBDOPTIONS=""
+## Type:           string
+## Default:        ""
+## ServiceRestart: winbind
+WINBINDOPTIONS=""
