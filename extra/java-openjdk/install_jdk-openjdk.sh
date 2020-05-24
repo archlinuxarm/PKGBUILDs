6fe10fb16b7c1a256a2785838dcde73a1de3d5f6
Kevin Mihelich <kevin@archlinuxarm.org>
2020-05-24 00:48:24 +0000

    extra/java-openjdk to 14.0.1.u7-1

--- extra/java-openjdk/install_jdk-openjdk.sh
+++ extra/java-openjdk/install_jdk-openjdk.sh
@@ -1,4 +1,4 @@
-THIS_JDK='java-13-openjdk'
+THIS_JDK='java-14-openjdk'
 
 fix_default() {
   if [ ! -x /usr/bin/java ]; then
--- extra/java-openjdk/install_jre-openjdk.sh
+++ extra/java-openjdk/install_jre-openjdk.sh
@@ -1,4 +1,4 @@
-THIS_JRE='java-13-openjdk'
+THIS_JRE='java-14-openjdk'
 
 fix_default() {
   if [ ! -x /usr/bin/java ]; then
