d414a574e3e5499300dc16e21dab46f4d395f926
Kevin Mihelich <kevin@archlinuxarm.org>
2020-09-24 22:48:40 +0000

    core/perl to 5.32.0-3

--- core/perl/perlbin.sh
+++ core/perl/perlbin.sh
@@ -2,11 +2,11 @@
 # https://wiki.archlinux.org/index.php/Perl_Policy#Binaries_and_scripts
 # Added /usr/bin/*_perl dirs for scripts
 
-[ -d /usr/bin/site_perl ] && PATH=$PATH:/usr/bin/site_perl
+[ -d /usr/bin/site_perl ] && append_path '/usr/bin/site_perl'
 
-[ -d /usr/bin/vendor_perl ] && PATH=$PATH:/usr/bin/vendor_perl
+[ -d /usr/bin/vendor_perl ] && append_path '/usr/bin/vendor_perl'
 
-[ -d /usr/bin/core_perl ] && PATH=$PATH:/usr/bin/core_perl
+[ -d /usr/bin/core_perl ] && append_path '/usr/bin/core_perl'
 
 export PATH
 
