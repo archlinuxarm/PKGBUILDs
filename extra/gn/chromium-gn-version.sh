0f43b4090eeddc42c7d1d89585e47a68d6a8d3b1
Kevin Mihelich <kevin@archlinuxarm.org>
2020-12-12 05:06:49 +0000

    removed extra/gn

--- extra/gn/chromium-gn-version.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -eo pipefail
-
-chromium_version=${1:-$(curl -s https://omahaproxy.appspot.com/linux)}
-
-curl -s https://chromium.googlesource.com/chromium/src/+/$chromium_version/DEPS?format=TEXT |
-	base64 -d | grep -Po "'gn_version': 'git_revision:\K[^']*"
