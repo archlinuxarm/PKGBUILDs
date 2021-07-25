2b9e339c5946345b7efbf59b660cadd6240603a3
Kevin Mihelich <kevin@archlinuxarm.org>
2021-07-25 13:09:22 +0000

    core/perl to 5.34.0-2

--- core/perl/generate-rebuild-list.sh
+++ core/perl/generate-rebuild-list.sh
@@ -6,6 +6,7 @@ echo "vim"
 
 pkgfile -rd "^/usr/lib/perl5/" | sed 's#^.*/##' | sort -u
 
+ssh build.archlinux.org sogrep -r
 for repo in core extra community multilib; do
 	ssh build.archlinux.org sogrep "$repo" libperl.so
 done
