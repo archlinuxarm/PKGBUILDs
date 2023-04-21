dd1c1f94a0ff3f9c12433e8a857bd066e92d8982
Kevin Mihelich <kevin@archlinuxarm.org>
2023-04-21 01:03:30 +0000

    extra/java-openjdk to 20.0.1.u9-2

--- extra/java-openjdk/install_jdk-openjdk.sh
+++ extra/java-openjdk/install_jdk-openjdk.sh
@@ -1,4 +1,4 @@
-THIS_JDK='java-19-openjdk'
+THIS_JDK='java-20-openjdk'
 
 fix_default() {
   if [ ! -x /usr/bin/java ]; then
--- extra/java-openjdk/install_jre-openjdk.sh
+++ extra/java-openjdk/install_jre-openjdk.sh
@@ -1,4 +1,4 @@
-THIS_JRE='java-19-openjdk'
+THIS_JRE='java-20-openjdk'
 
 fix_default() {
   if [ ! -x /usr/bin/java ]; then
