fe61e3e0d1d19397f72c5a0044b47edb81347108
Kevin Mihelich <kevin@archlinuxarm.org>
2024-03-15 13:10:56 +0000

    extra/libreoffice-fresh to 24.2.1-4

--- extra/libreoffice-fresh/libreoffice-fresh.csh
+++ extra/libreoffice-fresh/libreoffice-fresh.csh
@@ -5,8 +5,6 @@
 # to force a certain look'n feel
 
 #setenv SAL_USE_VCLPLUGIN gen
-#setenv SAL_USE_VCLPLUGIN gtk3_kde5
-#setenv SAL_USE_VCLPLUGIN kf5
 #setenv SAL_USE_VCLPLUGIN kf6
 #setenv SAL_USE_VCLPLUGIN qt5
 #setenv SAL_USE_VCLPLUGIN qt6
--- extra/libreoffice-fresh/libreoffice-fresh.sh
+++ extra/libreoffice-fresh/libreoffice-fresh.sh
@@ -5,8 +5,6 @@
 # to force a certain look'n feel
 
 #export SAL_USE_VCLPLUGIN=gen
-#export SAL_USE_VCLPLUGIN=gtk3_kde5
-#export SAL_USE_VCLPLUGIN=kf5
 #export SAL_USE_VCLPLUGIN=kf6
 #export SAL_USE_VCLPLUGIN=qt5
 #export SAL_USE_VCLPLUGIN=qt6
