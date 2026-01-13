deb7a18b19c7f2a137acb4009610528bb17e8f9b
Kevin Mihelich <kevin@archlinuxarm.org>
2026-01-13 01:10:14 +0000

    extra/intellij-idea-community-edition to 2025.3.1-1

--- extra/intellij-idea-community-edition/idea.sh
+++ extra/intellij-idea-community-edition/idea.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
+# Priority order for IDEA_JDK:
+# 1. Use IDEA_JDK if provided from external variable
+# 2. Use package JBR if it exists
+# 3. Fall back to /usr/lib/jvm/java-21-openjdk/
 if [ -z "$IDEA_JDK" ] ; then
-  IDEA_JDK="/usr/lib/jvm/java-21-openjdk/"
-fi
-# use JBR if exists
-if [ -d "/usr/share/idea/jbr/" ] ; then
-  IDEA_JDK="/usr/share/idea/jbr/"
+  if [ -d "/usr/share/idea/jbr/" ] ; then
+    IDEA_JDK="/usr/share/idea/jbr/"
+  else
+    IDEA_JDK="/usr/lib/jvm/java-21-openjdk/"
+  fi
 fi
 # open-jfx location that should match the JDK version
 if [ -z "$IDEA_JFX" ] ; then
