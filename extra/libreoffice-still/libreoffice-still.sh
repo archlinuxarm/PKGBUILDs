448d9d120cab66edfe9c4a8267da7d947ca5e428
Kevin Mihelich <kevin@archlinuxarm.org>
2026-02-11 18:57:15 +0000

    extra/libreoffice-still to 25.8.4-2

--- extra/libreoffice-still/libreoffice-still.sh
+++ extra/libreoffice-still/libreoffice-still.sh
@@ -6,7 +6,6 @@
 
 #export SAL_USE_VCLPLUGIN=gen
 #export SAL_USE_VCLPLUGIN=kf6
-#export SAL_USE_VCLPLUGIN=qt5
 #export SAL_USE_VCLPLUGIN=qt6
 #export SAL_USE_VCLPLUGIN=gtk3
 #export SAL_USE_VCLPLUGIN=gtk4
